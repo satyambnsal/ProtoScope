b15bf913dff84c48fb8e00bee2e707a8e5fb5f36
Satyam Bansal <satyamsgsits1994@gmail.com>
2024-11-17 05:17:35 +0700

    Code cleanup

--- src/components/ExplorerTabs.tsx
+++ src/components/ExplorerTabs.tsx
@@ -41,10 +41,10 @@ export function ExplorerTabs() {
                 Analytics
               </TabsTrigger>
             </TabsList>
-            <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
+            {/* <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
               <GearIcon className="mr-2 h-4 w-4" />
               Settings
-            </button>
+            </button> */}
             <TransactionSearch />
           </div>
         </div>
